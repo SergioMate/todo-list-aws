c7b47e3df6cf367c5e13c070a01425b0cda9d080
SergioMate <sergiomatecastro@gmail.com>
2022-01-29 23:01:08 +0100

    test fix

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -31,7 +31,6 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.uuid = "123e4567-e89b-12d3-a456-426614174000"
         self.text = "Aprender DevOps y Cloud en la UNIR"
 
-        from src.todoList import create_todo_table
         self.table = create_todo_table(self.dynamodb)
         #self.table_local = create_todo_table()
         print ('End: setUp')
@@ -228,6 +227,46 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertEqual("Apprenez DevOps et Cloud à l'UNIR", translation)
         "Apprenez DevOps et Cloud à l'UNIR"
         print ('End: test_delete_todo')
+
+
+def create_todo_table(dynamodb):
+    # For unit testing
+    tableName = os.environ['DYNAMODB_TABLE']
+    print('Creating Table with name:' + tableName)
+    table = dynamodb.create_table(
+        TableName=tableName,
+        KeySchema=[
+            {
+                'AttributeName': 'id',
+                'KeyType': 'HASH'
+            }''',
+            {
+                'AttributeName': 'language',
+                'KeyType': 'RANGE'
+            }'''
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'id',
+                'AttributeType': 'S'
+            }''',
+            {
+                'AttributeName': 'language',
+                'AttributeType': 'S'
+            }'''
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+        }
+    )
+
+    # Wait until the table exists.
+    table.meta.client.get_waiter('table_exists').wait(TableName=tableName)
+    if (table.table_status != 'ACTIVE'):
+        raise AssertionError()
+
+    return table
 
 
 if __name__ == '__main__':
