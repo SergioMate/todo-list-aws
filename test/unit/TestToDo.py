8f2c6f8121f931cb9892c3a76c0a73361add239c
SergioMate <sergiomatecastro@gmail.com>
2022-01-29 22:36:02 +0100

    translate lambda

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -214,6 +214,20 @@ class TestDatabaseFunctions(unittest.TestCase):
         self.assertRaises(TypeError, delete_item("", self.dynamodb))
         print ('End: test_delete_todo_error')
 
+    def test_translate_todo(self):
+        print ('---------------------')
+        print ('Start: test_translate_todo')
+        from src.todoList import translate_item
+        # Testing file functions
+        # Table mock
+        translation = translate_item(self.text, "en", self.dynamodb)
+        print ('Response translate en:' + str(translation))
+        self.assertEqual("Learn DevOps and Cloud at UNIR", translation)
+        translation = translate_item(self.text, "fr", self.dynamodb)
+        print ('Response translate fr:' + str(translation))
+        self.assertEqual("Apprenez DevOps et Cloud à l'UNIR", translation)
+        "Apprenez DevOps et Cloud à l'UNIR"
+        print ('End: test_delete_todo')
 
 
 if __name__ == '__main__':
