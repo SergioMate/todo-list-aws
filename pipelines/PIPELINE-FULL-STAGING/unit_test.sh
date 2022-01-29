78be076c7a0ddd212e8373090dcfd7299a149fc3
SergioMate <sergiomatecastro@gmail.com>
2022-01-29 14:31:09 +0100

    ENDPOINT_OVERRIDE nvironment variable added

--- pipelines/PIPELINE-FULL-STAGING/unit_test.sh
+++ pipelines/PIPELINE-FULL-STAGING/unit_test.sh
@@ -4,7 +4,8 @@ source todo-list-aws/bin/activate
 set -x
 export PYTHONPATH="${PYTHONPATH}:$(pwd)"
 echo "PYTHONPATH: $PYTHONPATH"
-export DYNAMODB_TABLE=todoUnitTestsTable
+export DYNAMODB_TABLE=local-TodosDynamoDbTable
+export ENDPOINT_OVERRIDE=http://dynamodb:8000
 python test/unit/TestToDo.py
 pip show coverage
 coverage run --include=src/todoList.py test/unit/TestToDo.py
